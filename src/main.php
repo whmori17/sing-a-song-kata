ff18fbaba5d182a2a1348dd4954a14d3593a8b8d
Gabriele Morisco <gabriele.morisco@facile.it>
2019-07-20 20:24:05 +0200

    Prime avvisaglie di refactoring

--- src/main.php
+++ src/main.php
@@ -1,35 +1,42 @@
 <?php
 
-$song = "There was an old lady who swallowed a fly.\n" .
+$song = new Song();
+
+$song->verses[0] = "There was an old lady who swallowed a fly.\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a spider;\n" .
+    "\n";
+
+$song->verses[1] = "There was an old lady who swallowed a spider;\n" .
     "That wriggled and wiggled and tickled inside her.\n" .
     "She swallowed the spider to catch the fly;\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a bird;\n" .
+    "\n";
+
+$song->verses[2] = "There was an old lady who swallowed a bird;\n" .
     "How absurd to swallow a bird.\n" .
     "She swallowed the bird to catch the spider,\n" .
     "She swallowed the spider to catch the fly;\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a cat;\n" .
+    "\n";
+
+$song->verses[3] = "There was an old lady who swallowed a cat;\n" .
     "Fancy that to swallow a cat!\n" .
     "She swallowed the cat to catch the bird,\n" .
     "She swallowed the bird to catch the spider,\n" .
     "She swallowed the spider to catch the fly;\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a dog;\n" .
+    "\n";
+
+$song->verses[4] = "There was an old lady who swallowed a dog;\n" .
     "What a hog, to swallow a dog!\n" .
     "She swallowed the dog to catch the cat,\n" .
     "She swallowed the cat to catch the bird,\n" .
     "She swallowed the bird to catch the spider,\n" .
     "She swallowed the spider to catch the fly;\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a cow;\n" .
+    "\n" ;
+
+$song->verses[5] = "There was an old lady who swallowed a cow;\n" .
     "I don't know how she swallowed a cow!\n" .
     "She swallowed the cow to catch the dog,\n" .
     "She swallowed the dog to catch the cat,\n" .
@@ -37,7 +44,9 @@ $song = "There was an old lady who swallowed a fly.\n" .
     "She swallowed the bird to catch the spider,\n" .
     "She swallowed the spider to catch the fly;\n" .
     "I don't know why she swallowed a fly - perhaps she'll die!\n" .
-    "\n" .
-    "There was an old lady who swallowed a horse...\n" .
+    "\n" ;
+
+$song->verses[6] = "There was an old lady who swallowed a horse...\n" .
     "...She's dead, of course!";
-echo $song;
+
+echo $song->toString();
