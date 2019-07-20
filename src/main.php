4cdbbea7d2738bd642c117efc2ae14bf2d5b3261
Gabriele Morisco <gabriele.morisco@facile.it>
2019-07-21 00:46:39 +0200

    Estratti animali

--- src/main.php
+++ src/main.php
@@ -2,17 +2,17 @@
 
 $song = new Song();
 
+$fly = "fly";
 $verses1 = [
-    new SwallewdAnimalVerse("fly", "."),
+    new SwallewdAnimalVerse($fly, "."),
     new Verse("I don't know why she swallowed a fly - perhaps she'll die!\n\n"),
 ];
 $strophe1 = new Strophe($verses1);
 $song->addStrophe($strophe1);
 
 $spider = "spider";
-$fly = "fly";
 $verses2 = [
-    new SwallewdAnimalVerse("spider" ),
+    new SwallewdAnimalVerse($spider),
     new Verse("That wriggled and wiggled and tickled inside her.\n" ),
     new SwallewdAndCatchAnimalVerse($spider, $fly),
     new Verse("I don't know why she swallowed a fly - perhaps she'll die!\n\n" ),
@@ -22,7 +22,7 @@ $song->addStrophe($strophe2);
 
 $bird = "bird";
 $verses3 = [
-    new SwallewdAnimalVerse("bird" ),
+    new SwallewdAnimalVerse($bird),
     new Verse("How absurd to swallow a bird.\n" ),
     new SwallewdAndCatchAnimalVerse($bird, $spider),
     new SwallewdAndCatchAnimalVerse($spider, $fly),
@@ -33,7 +33,7 @@ $song->addStrophe($strophe3);
 
 $cat = "cat";
 $verses4 = [
-    new SwallewdAnimalVerse("cat" ),
+    new SwallewdAnimalVerse($cat),
     new Verse("Fancy that to swallow a cat!\n" ),
     new SwallewdAndCatchAnimalVerse($cat, $bird),
     new SwallewdAndCatchAnimalVerse($bird, $spider),
@@ -45,7 +45,7 @@ $song->addStrophe($strophe4);
 
 $dog = "dog";
 $verses5 = [
-    new SwallewdAnimalVerse("dog" ),
+    new SwallewdAnimalVerse($dog),
     new Verse("What a hog, to swallow a dog!\n" ),
     new SwallewdAndCatchAnimalVerse($dog, $cat),
     new SwallewdAndCatchAnimalVerse($cat, $bird),
@@ -58,7 +58,7 @@ $song->addStrophe($strophe5);
 
 $cow = "cow";
 $verses6 = [
-    new SwallewdAnimalVerse("cow" ),
+    new SwallewdAnimalVerse($cow),
     new Verse("I don't know how she swallowed a cow!\n" ),
     new SwallewdAndCatchAnimalVerse($cow, $dog),
     new SwallewdAndCatchAnimalVerse($dog, $cat),
@@ -70,8 +70,9 @@ $verses6 = [
 $strophe6 = new Strophe($verses6);
 $song->addStrophe($strophe6);
 
+$horse = "horse";
 $verses7 = [
-    new SwallewdAnimalVerse("horse", "..." ),
+    new SwallewdAnimalVerse($horse, "..." ),
     new Verse("...She's dead, of course!"),
 ];
 $strophe7 = new Strophe($verses7);
