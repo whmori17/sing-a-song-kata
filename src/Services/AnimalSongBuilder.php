6e5e5a60a6cf56b7d1dfa236fbfdc5b92b9aac84
Gabriele Morisco <gabriele.morisco@facile.it>
2019-07-21 17:56:48 +0200

    Refactoring builder

--- src/Services/AnimalSongBuilder.php
+++ src/Services/AnimalSongBuilder.php
@@ -10,13 +10,18 @@ class AnimalSongBuilder
     /**
      * @var array
      */
-    private CONST VERSES_IN_THE_MIDDLE = [
+    private const VERSES_IN_THE_MIDDLE = [
         "That wriggled and wiggled and tickled inside her.\n",
         "How absurd to swallow a %s.\n",
         "Fancy that to swallow a %s!\n",
         "What a hog, to swallow a %s!\n",
         "I don't know how she swallowed a %s!\n",
     ];
+
+    /**
+     * @var string
+     */
+    private const SHE_S_DEAD_OF_COURSE = "...She's dead, of course!";
 
     /**
      * @var array
@@ -33,24 +38,24 @@ class AnimalSongBuilder
 
         $this->song = new Song();
 
-        $this->addFirstStropheToSong($animals, $dontKnowSwallewedAnimalVerse);
+        $this->addFirstStropheToSong($animals[0], $dontKnowSwallewedAnimalVerse);
 
         $this->addStrophesInTheMiddle($animals, $dontKnowSwallewedAnimalVerse);
 
-        $this->addFinalStrophe($animals);
+        $this->addFinalStrophe($animals[sizeof($animals) - 1]);
 
         return $this->song;
     }
 
     /**
-     * @param string $animal1
-     * @param string $animal2
+     * @param string $currentAnimal
+     * @param string $previousAnimal
      * @param Strophe $strophe
      * @return Strophe
      */
-    private function addAnimalsVerseToStrophe(string $animal1, string $animal2, Strophe $strophe): Strophe
+    private function addAnimalsVerseToStrophe(string $currentAnimal, string $previousAnimal, Strophe $strophe): Strophe
     {
-        $verse = new SwallowedAndCatchAnimalVerse($animal1, $animal2);
+        $verse = new SwallowedAndCatchAnimalVerse($currentAnimal, $previousAnimal);
         $strophe->addVerse($verse);
 
         foreach ($this->alreadyAddedAnimalsVerses as $addedAnimalsStrophe) {
@@ -71,14 +76,14 @@ class AnimalSongBuilder
     }
 
     /**
-     * @param array $animals
+     * @param string $firstAnimal
      * @param DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse
      */
-    protected function addFirstStropheToSong(array $animals, DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse): void
+    private function addFirstStropheToSong(string $firstAnimal, DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse): void
     {
         $this->addAndCreateStropheWithVerses([
-            new SwallowedAnimalVerse($animals[0], "."),
-            $dontKnowSwallewedAnimalVerse
+            new SwallowedAnimalVerse($firstAnimal, "."),
+            $dontKnowSwallewedAnimalVerse,
         ]);
     }
 
@@ -86,13 +91,15 @@ class AnimalSongBuilder
      * @param array $animals
      * @param DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse
      */
-    protected function addStrophesInTheMiddle(array $animals, DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse): void
+    private function addStrophesInTheMiddle(array $animals, DontKnowSwallowedAnimalVerse $dontKnowSwallewedAnimalVerse): void
     {
         for ($i = 1; $i < sizeof($animals) - 2; $i++) {
             $strophe = new Strophe();
+            $currentAnimal = $animals[$i];
+            $previousAnimal = $animals[$i - 1];
 
-            $strophe->addVerse(new Verse(sprintf(self::VERSES_IN_THE_MIDDLE[$i], $animals[$i])));
-            $strophe = $this->addAnimalsVerseToStrophe($animals[$i], $animals[$i - 1], $strophe);
+            $strophe->addVerse(new Verse(sprintf(self::VERSES_IN_THE_MIDDLE[$i], $currentAnimal)));
+            $strophe = $this->addAnimalsVerseToStrophe($currentAnimal, $previousAnimal, $strophe);
             $strophe->addVerse($dontKnowSwallewedAnimalVerse);
 
             $this->song->addStrophe($strophe);
@@ -100,12 +107,12 @@ class AnimalSongBuilder
     }
 
     /**
-     * @param array $animals
+     * @param string $lastAnimal
      */
-    protected function addFinalStrophe(array $animals): void
+    private function addFinalStrophe(string $lastAnimal): void
     {
-        $verseSemiFinal = new SwallowedAnimalVerse($animals[sizeof($animals) - 1], "...");
-        $finalVerse = new Verse("...She's dead, of course!");
+        $verseSemiFinal = new SwallowedAnimalVerse($lastAnimal, "...");
+        $finalVerse = new Verse(self::SHE_S_DEAD_OF_COURSE);
 
         $this->addAndCreateStropheWithVerses([
                 $verseSemiFinal,
